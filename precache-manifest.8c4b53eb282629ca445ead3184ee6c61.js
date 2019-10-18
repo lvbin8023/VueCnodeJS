d5028ce2ec63bb181a2c2f55c3476b7cd9d3ea4c
lvbin <472245767@qq.com>
2019-10-18 15:11:30 +0800

    项目代码优化

---
+++ precache-manifest.8c4b53eb282629ca445ead3184ee6c61.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/VueCnodeJS/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueCnodeJS/robots.txt"
+  },
+  {
+    "revision": "468db81c86e9d9590b0e",
+    "url": "/VueCnodeJS/js/chunk-vendors.1c2f29c8.js"
+  },
+  {
+    "revision": "0de93b90bcb184d3de00",
+    "url": "/VueCnodeJS/js/app.56a6bcf0.js"
+  },
+  {
+    "revision": "e573327bb87c07b0d16b68643769aa4f",
+    "url": "/VueCnodeJS/index.html"
+  },
+  {
+    "revision": "b296ff53120acd2ce313625524f9077d",
+    "url": "/VueCnodeJS/img/thumbs-up.b296ff53.svg"
+  },
+  {
+    "revision": "86f6a7c2f6cf9844d8a9f5d088d87406",
+    "url": "/VueCnodeJS/img/nodeJs.86f6a7c2.svg"
+  },
+  {
+    "revision": "078fcd0940076c44bf21d40b8cf5d3b1",
+    "url": "/VueCnodeJS/img/loading.078fcd09.gif"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/VueCnodeJS/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "468db81c86e9d9590b0e",
+    "url": "/VueCnodeJS/css/chunk-vendors.728eb7d9.css"
+  },
+  {
+    "revision": "0de93b90bcb184d3de00",
+    "url": "/VueCnodeJS/css/app.189ac403.css"
+  }
+];
